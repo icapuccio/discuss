ea47547a80bc46544da0ae987be87865c6080e80
Ignacio Capuccio <ignacio.capuccio@widergy.com>
2019-10-27 16:16:42 -0300

    Add Topics with CRUD features

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,8 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", TopicController, :index
+    resources "/topics", TopicController
   end
 
   # Other scopes may use custom stacks.
